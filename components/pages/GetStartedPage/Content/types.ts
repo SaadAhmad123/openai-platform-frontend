63f59a1c50179421d56f146ded95ce63fe74608b
Saad Ahmad <personal@saadahmad.dev>
2023-05-01 00:15:15 +1000

    udpTW

---
+++ components/pages/GetStartedPage/Content/types.ts
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export type GetStartedContent = {
+    component: React.FC,
+    label: string,
+    link: string,
+    labelIndex: string
+}
+
+export interface IContentComponent {
+    link: string
+}
